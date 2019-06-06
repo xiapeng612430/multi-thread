924d5af9c6edc1d81947e7c3a7f18cc30770070c
xianpeng.xia <951256916>
2019-06-06 11:44:57 +0800

    interrupted()测试当前线程是否是中断状态，并将状态置为false；isInterrupted（）测试线程是否是中断状态

---
+++ src/main/java/stopThread/RunTestInterrupted.java
@@ -0,0 +1,23 @@
+package stopThread;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-06 11:09
+ */
+public class RunTestInterrupted {
+
+    public static void main(String[] args) {
+        try {
+            MyThread myThread = new MyThread();
+            myThread.start();
+            Thread.sleep(1000);
+            myThread.interrupt();
+            System.out.println("是否停止1?" + Thread.interrupted());
+            System.out.println("是否停止2?" + Thread.interrupted());
+        } catch (InterruptedException e) {
+            System.out.println("main catch");
+            e.printStackTrace();
+        }
+        System.out.println("end");
+    }
+}
---
+++ src/main/java/stopThread/RunTestIsInterrupted.java
@@ -0,0 +1,23 @@
+package stopThread;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-06 11:36
+ */
+public class RunTestIsInterrupted {
+
+    public static void main(String[] args) {
+
+        try {
+            MyThread thread = new MyThread();
+            thread.start();
+            Thread.sleep(1000);
+            thread.interrupt();
+            System.out.println("是否停止1?" + thread.isInterrupted());
+            System.out.println("是否停止2?" + thread.isInterrupted());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
