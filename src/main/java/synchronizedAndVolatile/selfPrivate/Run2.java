9512dea9bf27a6031984eee1d70ef4efc1e7d8ff
xianpeng.xia <951256916>
2019-06-07 15:14:47 +0800

    synchronized锁的是对象

---
+++ src/main/java/synchronizedAndVolatile/selfPrivate/Run2.java
@@ -0,0 +1,17 @@
+package synchronizedAndVolatile.selfPrivate;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-07 15:12
+ */
+public class Run2 {
+
+    public static void main(String[] args) {
+        HasSelfPrivateNum num1 = new HasSelfPrivateNum();
+        HasSelfPrivateNum num2 = new HasSelfPrivateNum();
+        ThreadA threadA = new ThreadA(num1);
+        ThreadB threadB = new ThreadB(num2);
+        threadA.start();
+        threadB.start();
+    }
+}
