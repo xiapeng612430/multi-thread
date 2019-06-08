a8c480fa24f2bfd1fce51fe4a60d5b39e70b7ef2
xianpeng.xia <951256916>
2019-06-08 11:34:46 +0800

    使用synchronized（非this对象x），对象监视器必须是同一个对象

--- src/main/java/synchronizedAndVolatile/synchronized_/synBlockString/Service.java
+++ src/main/java/synchronizedAndVolatile/synchronized_/synBlockString/Service.java
@@ -8,18 +8,18 @@ public class Service {
 
     private String usernameParam;
     private String passwordParam;
-    private String anyString = new String();
 
     public void setUsernamePassword(String username, String password) {
         try {
-            synchronized (anyString){
-                System.out.println("thread name : "+Thread.currentThread().getName()+" in -> "+System.currentTimeMillis() +" enter synchronized block");
+            String anyString = new String();
+            synchronized (anyString) {
+                System.out.println("thread name : " + Thread.currentThread().getName() + " in -> " + System.currentTimeMillis() + " enter synchronized block");
                 usernameParam = username;
                 Thread.sleep(3000);
                 passwordParam = password;
-                System.out.println("thread name : "+Thread.currentThread().getName()+" in -> "+System.currentTimeMillis() +" leave synchronized block");
+                System.out.println("thread name : " + Thread.currentThread().getName() + " in -> " + System.currentTimeMillis() + " leave synchronized block");
             }
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
