28eeefc7a737e99b42d52947e13d20b7cb40bd1e
xianpeng.xia <951256916>
2019-06-08 13:18:52 +0800

    多个线程同时执行sunchronized(X){}同步代码块呈同步效果

--- src/main/java/synchronizedAndVolatile/synchronized_/synchronizedBlockLockAll/Run_1.java
+++ src/main/java/synchronizedAndVolatile/synchronized_/synchronizedBlockLockAll/Run_1.java
@@ -8,11 +8,12 @@ public class Run_1 {
 
     public static void main(String[] args) {
         Service service = new Service();
-        MyObject object = new MyObject();
-        ThreadA a = new ThreadA(service, object);
+        MyObject object1 = new MyObject();
+        MyObject object2 = new MyObject();
+        ThreadA a = new ThreadA(service, object1);
         a.setName("A");
         a.start();
-        ThreadB b = new ThreadB(service, object);
+        ThreadB b = new ThreadB(service, object2);
         b.setName("B");
         b.start();
     }
