55a46162c8361a80568f22bcba3e5734493ac2f4
xianpeng.xia <951256916>
2019-06-08 21:15:49 +0800

    死循环打印

---
+++ src/main/java/synchronizedAndVolatile/volatile_/printStringTest_1/PrintString.java
@@ -0,0 +1,29 @@
+package synchronizedAndVolatile.volatile_.printStringTest_1;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-08 21:08
+ */
+public class PrintString {
+
+    private boolean isContinuePrint = true;
+
+    public boolean isContinuePrint() {
+        return isContinuePrint;
+    }
+
+    public void setContinuePrint(boolean continuePrint) {
+        isContinuePrint = continuePrint;
+    }
+
+    public void printStringMethod() {
+        try {
+            while (isContinuePrint == true) {
+                System.out.println("run printStringMethod threadName = " + Thread.currentThread().getName());
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
