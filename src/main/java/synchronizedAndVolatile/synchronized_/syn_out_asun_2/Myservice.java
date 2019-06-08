0c0aa04fd1f313720ec6005da626fc63bee1852e
xianpeng.xia <951256916>
2019-06-08 12:31:20 +0800

    synchronized 同步方法脏读;处理方式是synchronized(list)

--- src/main/java/synchronizedAndVolatile/synchronized_/syn_out_asun_2/Myservice.java
+++ src/main/java/synchronizedAndVolatile/synchronized_/syn_out_asun_2/Myservice.java
@@ -8,9 +8,11 @@ public class Myservice {
 
     public MyOneList addServiceMethod(MyOneList list, String data) {
         try {
-            if (list.getSize() < 1) {
-                Thread.sleep(2000);
-                list.add(data);
+            synchronized (list) {
+                if (list.getSize() < 1) {
+                    Thread.sleep(2000);
+                    list.add(data);
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
