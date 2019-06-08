a79f96f01e1a25c70f832e61b92839689be4ae11
xianpeng.xia <951256916>
2019-06-09 01:44:15 +0800

    wait()

---
+++ src/main/java/Inter_threadCommunication/waitAndNotify/Test2.java
@@ -0,0 +1,23 @@
+package Inter_threadCommunication.waitAndNotify;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-09 01:40
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        try {
+            String lock = new String();
+            System.out.println("before syn");
+            synchronized (lock){
+                System.out.println("first row in syn");
+                lock.wait();
+                System.out.println("after wait first row");
+            }
+            System.out.println("after syn first row");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
