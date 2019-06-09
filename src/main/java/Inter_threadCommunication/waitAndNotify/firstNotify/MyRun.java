86de84f501c8e43adf27b50a2c050c66acfff146
xianpeng.xia <951256916>
2019-06-09 10:44:08 +0800

    notify()过早，wait()没必要执行了

---
+++ src/main/java/Inter_threadCommunication/waitAndNotify/firstNotify/MyRun.java
@@ -0,0 +1,44 @@
+package Inter_threadCommunication.waitAndNotify.firstNotify;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-09 10:35
+ */
+public class MyRun {
+
+    private String lock = new String("");
+    private Runnable runnableA = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                synchronized (lock) {
+                    System.out.println("begin wait");
+                    lock.wait();
+                    System.out.println("end   wait");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private Runnable runnableB = new Runnable() {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                System.out.println("begin notify");
+                lock.notify();
+                System.out.println("end   notify");
+            }
+        }
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+        MyRun run = new MyRun();
+        Thread b = new Thread(run.runnableB);
+        b.start();
+        Thread.sleep(100);
+        Thread a = new Thread(run.runnableA);
+        a.start();
+    }
+}
