23e3ef341a523d9f366eff69a223d58831f906ee
xianpeng.xia <951256916>
2019-06-09 11:12:31 +0800

    等待wait的条件发生变化，容易造成程序逻辑混乱,改if为while

--- src/main/java/Inter_threadCommunication/waitAndNotify/waitOld/Subtract.java
+++ src/main/java/Inter_threadCommunication/waitAndNotify/waitOld/Subtract.java
@@ -16,7 +16,7 @@ public class Subtract {
     public void subtract() {
         try {
             synchronized (lock) {
-                if (ValueObject.list.size() == 0) {
+                while (ValueObject.list.size() == 0) {
                     System.out.println("wait begin ThreadName = " + Thread.currentThread().getName());
                     lock.wait();
                     System.out.println("wait   end ThreadName = " + Thread.currentThread().getName());
