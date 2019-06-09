32135ea2615e7674265fbcd2edfadcbcb273bf4c
xianpeng.xia <951256916>
2019-06-10 00:31:26 +0800

    InheritableThreadLocal：让子线程从父线程取得值

---
+++ src/main/java/Inter_threadCommunication/threadLocal/InheritableThreadLocal/Tools.java
@@ -0,0 +1,10 @@
+package Inter_threadCommunication.threadLocal.InheritableThreadLocal;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-10 00:26
+ */
+public class Tools {
+
+    public static InheritableThreadLocalExt t1 = new InheritableThreadLocalExt();
+}
