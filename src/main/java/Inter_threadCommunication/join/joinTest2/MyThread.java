36db1d9e003dd3f8ec72bc75d691992030215159
xianpeng.xia <951256916>
2019-06-09 22:59:09 +0800

    join (一个线程执行完，在执行主线程)

---
+++ src/main/java/Inter_threadCommunication/join/joinTest2/MyThread.java
@@ -0,0 +1,19 @@
+package Inter_threadCommunication.join.joinTest2;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-09 22:51
+ */
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        try {
+            int secondValue = (int) (Math.random() * 10000);
+            System.out.println(secondValue);
+            Thread.sleep(secondValue);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Inter_threadCommunication/join/joinTest2/Test.java
@@ -0,0 +1,19 @@
+package Inter_threadCommunication.join.joinTest2;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-09 22:53
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        try {
+            MyThread threadTest = new MyThread();
+            threadTest.start();
+            threadTest.join();
+            System.out.println("我想等 threadTest 执行完执行 ,我做到了");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
