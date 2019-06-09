956fdf6ebccddad564899bd5b06a60ccaa9bc8e8
xianpeng.xia <951256916>
2019-06-09 10:03:43 +0800

    notify()一次只随机通知一个线程进行唤醒

---
+++ src/main/java/Inter_threadCommunication/waitAndNotify/notifyOne/Service.java
@@ -0,0 +1,20 @@
+package Inter_threadCommunication.waitAndNotify.notifyOne;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-09 09:50
+ */
+public class Service {
+
+    public void testMethod(Object lock) {
+        try {
+            synchronized (lock) {
+                System.out.println("begin wait() Thread name = " + Thread.currentThread().getName());
+                lock.wait();
+                System.out.println("end   wait() Thread name = " + Thread.currentThread().getName());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
