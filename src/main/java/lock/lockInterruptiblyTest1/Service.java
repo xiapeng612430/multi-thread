1ba9140b3abe00836a279e3da57dec0a4092b2bb
xianpeng.xia <951256916>
2019-06-15 23:58:36 +0800

    使用lock()同步，执行interrupt（）不抛出异常

---
+++ src/main/java/lock/lockInterruptiblyTest1/Service.java
@@ -0,0 +1,32 @@
+package lock.lockInterruptiblyTest1;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-15 23:36
+ */
+public class Service {
+
+    public ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void waitMethod() {
+        try {
+            lock.lock();
+            System.out.println("lock begin " + Thread.currentThread().getName());
+            for (int i = 0; i < Integer.MAX_VALUE / 10; i++) {
+                String str = new String();
+                Math.random();
+                System.out.println("lock :" + Thread.currentThread().getName() +" print " +i);
+            }
+            System.out.println("lock end " + Thread.currentThread().getName());
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+
+}
