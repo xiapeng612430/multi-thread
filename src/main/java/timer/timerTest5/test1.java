2ddee092558b95f8e6d0f9165498deaf03c32dc3
xianpeng.xia <951256916>
2019-06-16 21:20:56 +0800

    schedule()任务不延迟的情况，下次任务开始时间=上次任务开始时间+period时间

---
+++ src/main/java/timer/timerTest5/test1.java
@@ -0,0 +1,35 @@
+package timer.timerTest5;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-16 21:16
+ */
+public class test1 {
+
+    static class MyTask extends TimerTask {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println("begin timer = " + System.currentTimeMillis());
+                Thread.sleep(1000);
+                System.out.println("end   timer = " + System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Calendar calendarRef = Calendar.getInstance();
+        Date runDate = calendarRef.getTime();
+        Timer timer = new Timer();
+        timer.schedule(task,runDate,3000);
+    }
+}
