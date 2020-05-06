f08c3aa3a4184198386996b7db2815f63267a373
夏先鹏 <951256916>
2020-05-06 23:01:04 +0800

    Phaser 类arriveAndAwaitAdvance()方法：当前线程已经到达屏障，在此等待一段时间，条件满足后继续往下一个屏障执行

---
+++ src/main/java/phaser/phaser_test1/ThreadA.java
@@ -0,0 +1,22 @@
+package phaser.phaser_test1;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/5/6 10:24 下午
+ */
+public class ThreadA extends Thread {
+
+    private Phaser phaser;
+
+    public ThreadA(Phaser phaser) {
+        super();
+        this.phaser = phaser;
+    }
+
+    @Override
+    public void run() {
+        PrintTools.methodA();
+    }
+}
