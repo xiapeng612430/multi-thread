f341c60be1ed91b262cce9a5b36fcb3d067719d4
夏先鹏 <951256916>
2020-05-06 23:20:55 +0800

    Phaser类的arriveAndDeregister()方法使当前线程退出，并使parties减1

---
+++ src/main/java/phaser/phaser_test2/PrintTools.java
@@ -0,0 +1,40 @@
+package phaser.phaser_test2;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/5/6 10:08 下午
+ */
+public class PrintTools {
+
+    public static Phaser phaser;
+
+    public static void methodA() {
+        System.out.println(Thread.currentThread().getName() + " A1 begin = " + System.currentTimeMillis());
+        phaser.arriveAndAwaitAdvance();
+        System.out.println(Thread.currentThread().getName() + " A1   end = " + System.currentTimeMillis());
+
+        System.out.println(Thread.currentThread().getName() + " A2 begin = " + System.currentTimeMillis());
+        phaser.arriveAndAwaitAdvance();
+        System.out.println(Thread.currentThread().getName() + " A2   end = " + System.currentTimeMillis());
+
+    }
+
+    public static void methodB() {
+        try {
+            System.out.println(Thread.currentThread().getName() + " A1 begin = " + System.currentTimeMillis());
+            Thread.sleep(5000);
+            phaser.arriveAndAwaitAdvance();
+            System.out.println("A:" + phaser.getRegisteredParties());
+
+            phaser.arriveAndDeregister();
+            System.out.println("B:" + phaser.getRegisteredParties());
+
+            System.out.println(Thread.currentThread().getName() + " A1   end = " + System.currentTimeMillis());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/phaser/phaser_test2/ThreadB.java
@@ -0,0 +1,22 @@
+package phaser.phaser_test2;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/5/6 10:24 下午
+ */
+public class ThreadB extends Thread {
+
+    private Phaser phaser;
+
+    public ThreadB(Phaser phaser) {
+        super();
+        this.phaser = phaser;
+    }
+
+    @Override
+    public void run() {
+        PrintTools.methodA();
+    }
+}
