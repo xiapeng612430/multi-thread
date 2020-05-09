0e52d8b4245f845276fb810ef7167868e6b0b305
夏先鹏 <951256916>
2020-05-09 10:27:07 +0800

    awaitAdvanceInterruptibly(int,long,TimeUnit)方法 在指定时间，栏数不变，则出现异常，否则继续执行

--- src/main/java/phaser/phaser_test8_3/Run.java
+++ src/main/java/phaser/phaser_test8_3/Run.java
@@ -8,10 +8,21 @@ import java.util.concurrent.Phaser;
  */
 public class Run {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Phaser phaser = new Phaser(3);
         ThreadA threadA = new ThreadA(phaser);
         threadA.setName("A");
         threadA.start();
+
+        Thread.sleep(1000);
+        phaser.arrive();
+
+        Thread.sleep(1000);
+        phaser.arrive();
+
+        Thread.sleep(1000);
+        phaser.arrive();
+
+        System.out.println(System.currentTimeMillis());
     }
 }
