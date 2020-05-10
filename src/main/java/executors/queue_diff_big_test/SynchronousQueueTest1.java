732d447961036d4fbd3238ecf2764644eb0a2e71
夏先鹏 <951256916>
2020-05-10 12:12:54 +0800

    使用SynchronousQueue 与max值的效果

---
+++ src/main/java/executors/queue_diff_big_test/SynchronousQueueTest1.java
@@ -0,0 +1,24 @@
+package executors.queue_diff_big_test;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/5/10 12:08 下午
+ */
+public class SynchronousQueueTest1 {
+
+    public static void main(String[] args) {
+        SynchronousQueue synchronousQueue = new SynchronousQueue<>();
+        System.out.println(synchronousQueue.size());
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 3, 5, TimeUnit.SECONDS, synchronousQueue);
+
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+
+        System.out.println(threadPoolExecutor.getPoolSize() + " " + synchronousQueue.size());
+    }
+}
