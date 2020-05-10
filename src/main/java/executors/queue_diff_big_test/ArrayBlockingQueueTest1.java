6f8620756d3ed71dbe79242c32779aaf513949d7
夏先鹏 <951256916>
2020-05-10 12:07:23 +0800

    使用ArrayBlockingDeque设置队列长度 与max值的效果

---
+++ src/main/java/executors/queue_diff_big_test/ArrayBlockingQueueTest1.java
@@ -0,0 +1,28 @@
+package executors.queue_diff_big_test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xianpeng.xia
+ * on 2020/5/10 12:01 下午
+ */
+public class ArrayBlockingQueueTest1 {
+
+    public static void main(String[] args) {
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue<>(2);
+        System.out.println(arrayBlockingQueue.size());
+
+        // max值被参考
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2, 3, 5, TimeUnit.SECONDS, arrayBlockingQueue);
+
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+        threadPoolExecutor.execute(new MyRunnable());
+
+        System.out.println(threadPoolExecutor.getPoolSize() + " " + arrayBlockingQueue.size());
+    }
+}
