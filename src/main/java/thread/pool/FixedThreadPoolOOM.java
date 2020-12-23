023efe868fe83c2e1c4aa5a60365c9591a5eef3f
夏先鹏 <951256916>
2020-12-24 01:30:24 +0800

    newFixedThreadPool 内存溢出

---
+++ src/main/java/thread/pool/FixedThreadPoolOOM.java
@@ -0,0 +1,32 @@
+package thread.pool;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author xianpeng.xia
+ * on 2020/12/24 上午1:20
+ */
+public class FixedThreadPoolOOM {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            executorService.execute(new SubThread());
+        }
+    }
+}
+
+class SubThread implements Runnable {
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
